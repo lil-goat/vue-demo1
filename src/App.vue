58cb1578da6ed54a724641952d7257025104a327
goat <1211928420@qq.com>
2025-10-19 17:27:16 +0800

    路由的使用

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="footer_wrap">
+      <a href="#/find">发现音乐</a>
+      <a href="#/my">我的音乐</a>
+      <a href="#/friend">朋友</a>
+    </div>
+    <div class="top">
+      <!-- 路由出口 → 匹配的组件所展示的位置 -->
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  margin: 0;
+  padding: 0;
+}
+.footer_wrap {
+  position: relative;
+  left: 0;
+  top: 0;
+  display: flex;
+  width: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #333;
+  color: #ccc;
+}
+.footer_wrap a {
+  flex: 1;
+  text-decoration: none;
+  padding: 20px 0;
+  line-height: 20px;
+  background-color: #333;
+  color: #ccc;
+  border: 1px solid black;
+}
+.footer_wrap a:hover {
+  background-color: #555;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 路由的使用步骤 5 + 2
+// 5个基础步骤
+// 1. 下载 v3.6.5
+// 2. 引入
+// 3. 安装注册 Vue.use(Vue插件)
+// 4. 创建路由对象
+// 5. 注入到new Vue中，建立关联
+
+// 2个核心步骤
+// 1. 建组件(views目录)，配规则
+// 2. 准备导航链接，配置路由出口(匹配的组件展示的位置) 
+import VueRouter from 'vue-router'
+import Find from './views/Find.vue'
+import Friend from './views/Friend.vue'
+import My from './views/My.vue'
+
+Vue.use(VueRouter) // VueRouter插件初始化
+
+const router = new VueRouter({
+  // routes 路由规则们
+  // route  一条路由规则 { path: 路径, component: 组件 }
+  routes: [
+    { path: '/find', component: Find },
+    { path: '/friend', component: Friend },
+    { path: '/my', component: My },
+  ]
+})
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
